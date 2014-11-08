1e15d26368c142c5f171169d56969fdf4f81c4bd
iwane <iwane@jp.fujitsu.com>
2014-11-09 01:59:25 +0900

    stack: error check

--- stack.go
+++ stack.go
@@ -1,10 +1,17 @@
 package qeconv
 
+import (
+	"errors"
+)
+
 type Stack struct {
 	v []Node
 }
 
 func (s *Stack) pop() Node {
+	if len(s.v) <= 0 {
+		errors.New("empty stack")
+	}
 	v := s.v[len(s.v)-1]
 	s.v = s.v[:len(s.v)-1]
 	return v
